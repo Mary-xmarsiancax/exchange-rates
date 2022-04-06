4c52269efe5a7a4f53450b1f024b7ee663cf5555
marsianca <xmarsiancax@mail.ru>
2022-04-06 10:53:41 +0300

    пофиксила css

--- src/index.tsx
+++ src/index.tsx
@@ -8,12 +8,9 @@ import store from "./store/redux-store";
 
 ReactDOM.render(
     <Provider store={store}>
-    <App />
+    <App/>
     </Provider>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
